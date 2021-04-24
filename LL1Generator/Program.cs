d3e1c94673bbb142e35ad4cd31a67df9f450db42
Dekhant <sasha.dekhant@gmail.com>
2021-04-24 13:20:38 +0300

    start

---
+++ LL1Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LL1Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

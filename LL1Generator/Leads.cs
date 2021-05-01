db2236a5c550551c607f113c000ef53bd3ae2355
Roman <carapacik@gmail.com>
2021-05-01 18:20:14 +0300

    cleanup

--- LL1Generator/Leads.cs
+++ LL1Generator/Leads.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using LL1Generator.Entities;
 
@@ -9,72 +8,60 @@ namespace LL1Generator
     {
         private static void FindUpRules(RuleList ruleList, string nonTerm, ref List<List<RuleItem>> leads)
         {
-            foreach(var rule in ruleList.Rules)
-            {
-                foreach(var item in rule.Items)
-                {
-                    if(item.Value == nonTerm)
-                    {
-                        if(rule.Items.IndexOf(item) == (rule.Items.Count - 1))
-                        {
-                            FindUpRules(ruleList, rule.NonTerminal, ref leads);
-                        }
-                        else
-                        {
-                            var lead = new List<RuleItem>();
-                            lead.Add(rule.Items[rule.Items.IndexOf(item) + 1]);
-                            leads.Add(lead);
-                        }
-                    }
-                }
-            }
-        }
-        public static List<List<RuleItem>> FindLeads(RuleList ruleList)
-        {
-            var leads = new List<List<RuleItem>>();
             foreach (var rule in ruleList.Rules)
-            {
-                if(rule.Items[0].Value == Constants.EmptySymbol)
+            foreach (var item in rule.Items.Where(item => item.Value == nonTerm))
+                if (rule.Items.IndexOf(item) == rule.Items.Count - 1)
                 {
                     FindUpRules(ruleList, rule.NonTerminal, ref leads);
                 }
                 else
                 {
-                    var lead = new List<RuleItem>();
-                    lead.Add(rule.Items[0]);
+                    var lead = new List<RuleItem> {rule.Items[rule.Items.IndexOf(item) + 1]};
                     leads.Add(lead);
                 }
-            }
-            bool foundNonTerm = true;
+        }
+
+        public static List<List<RuleItem>> FindLeads(RuleList ruleList)
+        {
+            var leads = new List<List<RuleItem>>();
+            foreach (var rule in ruleList.Rules)
+                if (rule.Items[0].Value == Constants.EmptySymbol)
+                {
+                    FindUpRules(ruleList, rule.NonTerminal, ref leads);
+                }
+                else
+                {
+                    var lead = new List<RuleItem> {rule.Items[0]};
+                    leads.Add(lead);
+                }
+
+            var foundNonTerm = true;
             while (foundNonTerm)
             {
-                var uniqueEnterances = new HashSet<List<RuleItem>>();
+                var uniqueEntrances = new HashSet<List<RuleItem>>();
                 foundNonTerm = false;
                 foreach (var lead in leads)
-                {          
-                    foreach (var leadSymbol in lead.ToList())
+                foreach (var leadSymbol in lead.ToList().Where(leadSymbol => !leadSymbol.IsTerminal))
+                {
+                    foundNonTerm = true;
+                    foreach (var rule in ruleList.Rules.Where(x => x.NonTerminal == leadSymbol.Value))
                     {
-                        if (!leadSymbol.IsTerminal)
+                        var index = ruleList.Rules.IndexOf(rule);
+                        if (!uniqueEntrances.Contains(leads[index]))
                         {
-                            foundNonTerm = true;
-                            foreach (var rule in ruleList.Rules.Where(x => x.NonTerminal == leadSymbol.Value))
-                            {
-                                int index = ruleList.Rules.IndexOf(rule);
-                                if (!uniqueEnterances.Contains(leads[index]))
-                                {
-                                    uniqueEnterances.Add(leads[index]);   
-                                    lead.AddRange(leads[index]);
-                                }
-                                else
-                                {
-                                    return null;
-                                }
-                            }
-                            lead.RemoveAt(lead.IndexOf(leadSymbol));
+                            uniqueEntrances.Add(leads[index]);
+                            lead.AddRange(leads[index]);
+                        }
+                        else
+                        {
+                            return null;
                         }
                     }
+
+                    lead.RemoveAt(lead.IndexOf(leadSymbol));
                 }
             }
+
             return leads;
         }
     }
